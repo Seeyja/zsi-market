bbe19748bb819b474300351c3a801493a25e4ef3
szpakusik <szpakusik@gmail.com>
2019-05-09 09:04:53 +0200

    res.render working after adding the user with alert()

--- app.js
+++ app.js
@@ -33,15 +33,55 @@ app.use( bodyParser.urlencoded( {extended: true} ) );
 
 // Getting data from POST method
 app.post('/', function (req, res) {
-    const data = {
-        user: req.body.login,
+
+    let grade = req.body.class;
+    let description = req.body.description;
+    let code;
+    do{
+      code = Math.floor(Math.random()*1000000);
+    }while(code < 100000)
+
+    let userData = {
+        username: req.body.login,
         num: req.body.num,
         email: req.body.email,
-        description: req.body.description,
-        class: req.body.class,
-        
-    };
-    res.send(data);
+        code: code,
+    }
+    let book_types = {
+        polish: req.body.polish+String(req.body.class),
+        english: req.body.english,
+        german: req.body.german,
+        history: req.body.history,
+        entrepreneurship: req.body.entrepreneurship,
+        geography: req.body.geography,
+        biology: req.body.biology,
+        chemistry: req.body.chemistry,
+        physics: req.body.physics,
+        math: req.body.math,
+        safety: req.body.safety,
+        religion: req.body.religion,
+        professionalCourses: req.body.professionalCourses,
+        society: req.body.society,
+        culture: req.body.culture,
+      }
+
+      // Look for existing user
+      let sql = `SELECT * FROM users WHERE username = "${userData.username}" OR num = "${userData.num}" OR email="${userData.email}"`;
+      let query = db.query(sql, (err, results)=>{
+        if(err) throw err;
+        console.log(results);
+      // If no user found, add new
+        if (results.length < 1) {
+            let sql = 'INSERT INTO users SET ?';
+            let query = db.query(sql, userData, (err, result)=>{
+              if(err) throw err;
+              console.log(result);
+              res.render( 'index', {hint: 'User added...'} );
+            });
+        }
+        else res.render( 'index', {hint: 'There is already user like that!'} );
+      });
+
 });
 
 // Route to index
