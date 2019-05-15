61d6b8e60941299d192a58d239c582a54869236b
szpakusik <szpakusik@gmail.com>
2019-05-15 16:11:52 +0200

    INSERT INTO SETS | fix

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -48,3 +48,80 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     }
 });
+
+/*********** Active category***********/
+
+const categories = document.querySelectorAll(".category img");
+
+categories.forEach(function (category) {
+    category.addEventListener('click', activeCategory);
+})
+
+function activeCategory() {
+    this.classList.toggle("active");
+}
+
+/*****Price range ***************/
+
+var select = document.getElementById('input-select');
+
+// Append the option elements
+for (var i = 0; i <= 100; i++) {
+
+    var option = document.createElement("option");
+    option.text = i;
+    option.value = i;
+
+    select.appendChild(option);
+}
+
+
+var html5Slider = document.getElementById('html5');
+
+noUiSlider.create(html5Slider, {
+    start: [0, 100],
+    connect: true,
+    range: {
+        'min': 0,
+        'max': 100
+    }
+});
+
+var inputNumber = document.getElementById('input-number');
+
+html5Slider.noUiSlider.on('update', function (values, handle) {
+
+    var value = values[handle];
+
+    if (handle) {
+        inputNumber.value = Math.round(value);
+    } else {
+        select.value = Math.round(value);
+    }
+});
+
+select.addEventListener('change', function () {
+    html5Slider.noUiSlider.set([this.value, null]);
+});
+
+inputNumber.addEventListener('change', function () {
+    html5Slider.noUiSlider.set([null, this.value]);
+});
+
+
+/**  BOOKS BUTTONS */
+function openWindow(evt, type) {
+    var i, window, buttons;
+    window = document.querySelectorAll(".window");
+    for (i = 0; i < window.length; i++) {
+        window[i].style.display = "none";
+    }
+    buttons = document.querySelectorAll(".inner_row button");
+    for (i = 0; i < buttons.length; i++) {
+        buttons[i].className = buttons[i].className.replace(" active", "");
+    }
+    document.getElementById(type).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+document.querySelector("#defaultOpen").click();
