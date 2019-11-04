10bddca4f41b402602272a1e75e09281c51458b4
szpakusik <szpakusik@gmail.com>
2019-11-04 07:23:13 +0100

    code correction

---
+++ controllers/connectionController-!EXAMPLE.js
@@ -0,0 +1,30 @@
+﻿const mysql = require( 'mysql' );
+const queryController = require('./queryController.js');
+const fs = require('fs');
+const multer = require('multer');
+
+//const tymczasowyController = require('./tymczasowyController');
+
+const upload = multer().none();
+
+//Create connection
+const db = mysql.createConnection({
+    host     : 'mysql12.mydevil.net',
+    user     : 'example',
+    password : 'passexample',
+    database : 'bookexample'
+});
+
+// Connect
+db.connect( ( err ) => {
+    if( err ) throw err;
+    console.log( 'Connected' );
+});
+
+module.exports = {
+
+  db: db,
+  
+
+
+}//module.exports end
